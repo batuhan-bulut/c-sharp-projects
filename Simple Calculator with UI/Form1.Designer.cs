5210852533d902461c2bcaf3f1ad899b26282b09
Batuhan Bulut <88343720+bulutbatuhan@users.noreply.github.com>
2022-07-17 18:45:41 +0300

    v1

---
+++ Simple Calculator with UI/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Simple_Calculator_with_UI
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.NumberBox1 = new System.Windows.Forms.TextBox();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.ButtonPlus = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.jobLabel = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NumberBox1
+            // 
+            this.NumberBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            resources.ApplyResources(this.NumberBox1, "NumberBox1");
+            this.NumberBox1.Name = "NumberBox1";
+            // 
+            // Num1
+            // 
+            resources.ApplyResources(this.Num1, "Num1");
+            this.Num1.Name = "Num1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
+            // 
+            // Num2
+            // 
+            resources.ApplyResources(this.Num2, "Num2");
+            this.Num2.Name = "Num2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
+            // 
+            // Num3
+            // 
+            resources.ApplyResources(this.Num3, "Num3");
+            this.Num3.Name = "Num3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
+            // 
+            // Num6
+            // 
+            resources.ApplyResources(this.Num6, "Num6");
+            this.Num6.Name = "Num6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
+            // 
+            // Num5
+            // 
+            resources.ApplyResources(this.Num5, "Num5");
+            this.Num5.Name = "Num5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
+            // 
+            // Num4
+            // 
+            resources.ApplyResources(this.Num4, "Num4");
+            this.Num4.Name = "Num4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
+            // 
+            // Num9
+            // 
+            resources.ApplyResources(this.Num9, "Num9");
+            this.Num9.Name = "Num9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
+            // 
+            // Num8
+            // 
+            resources.ApplyResources(this.Num8, "Num8");
+            this.Num8.Name = "Num8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
+            // 
+            // Num7
+            // 
+            resources.ApplyResources(this.Num7, "Num7");
+            this.Num7.Name = "Num7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
+            // 
+            // Equal
+            // 
+            resources.ApplyResources(this.Equal, "Equal");
+            this.Equal.Name = "Equal";
+            this.Equal.UseVisualStyleBackColor = true;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // Num0
+            // 
+            resources.ApplyResources(this.Num0, "Num0");
+            this.Num0.Name = "Num0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // ButtonPlus
+            // 
+            resources.ApplyResources(this.ButtonPlus, "ButtonPlus");
+            this.ButtonPlus.Name = "ButtonPlus";
+            this.ButtonPlus.UseVisualStyleBackColor = true;
+            // 
+            // Divide
+            // 
+            resources.ApplyResources(this.Divide, "Divide");
+            this.Divide.Name = "Divide";
+            this.Divide.UseVisualStyleBackColor = true;
+            this.Divide.Click += new System.EventHandler(this.Divide_Click);
+            // 
+            // Multiply
+            // 
+            resources.ApplyResources(this.Multiply, "Multiply");
+            this.Multiply.Name = "Multiply";
+            this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.Click += new System.EventHandler(this.Multiply_Click);
+            // 
+            // Minus
+            // 
+            resources.ApplyResources(this.Minus, "Minus");
+            this.Minus.Name = "Minus";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.BackColor = System.Drawing.SystemColors.ButtonFace;
+            resources.ApplyResources(this.Plus, "Plus");
+            this.Plus.Name = "Plus";
+            this.Plus.UseVisualStyleBackColor = false;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // jobLabel
+            // 
+            resources.ApplyResources(this.jobLabel, "jobLabel");
+            this.jobLabel.BackColor = System.Drawing.Color.DimGray;
+            this.jobLabel.Name = "jobLabel";
+            // 
+            // textBoxNumber
+            // 
+            resources.ApplyResources(this.textBoxNumber, "textBoxNumber");
+            this.textBoxNumber.Name = "textBoxNumber";
+            // 
+            // Calculator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.jobLabel);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.ButtonPlus);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.NumberBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Calculator";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox NumberBox1;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Equal;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button ButtonPlus;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Label jobLabel;
+        private System.Windows.Forms.TextBox textBoxNumber;
+    }
+}
+
---
+++ Simple Calculator with UI/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Calculator_with_UI
+{
+
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+       
+        int result;
+        int job;
+        int number1;
+        private bool isJobDone = false;
+        private void Num1_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num1.Text;
+            if (isJobDone)
+            {
+                NumberBox1.Text = null;
+            }
+        }
+        private void Num2_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num2.Text;
+        }
+        private void Num3_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num3.Text;
+        }
+        private void Num4_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num4.Text;
+        }
+        private void Num5_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num5.Text;
+        }
+        private void Num6_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num6.Text;
+        }
+        private void Num7_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num7.Text;
+        }
+        private void Num8_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num8.Text;
+        }
+        private void Num9_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num9.Text;
+        }
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            NumberBox1.Text = NumberBox1.Text + Num0.Text;
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            job = 0;
+            jobLabel.Text = "It's Plus";
+            if (!(NumberBox1.Text == "")) { number1 = Convert.ToInt32(NumberBox1.Text); }
+            textBoxNumber.Text = Convert.ToString(number1);
+            NumberBox1.Text = null;
+            if (job == 0)
+            {
+                Plus.BackColor = Color.Aqua;
+                Minus.BackColor = Color.DimGray;
+                Multiply.BackColor = Color.DimGray;
+                Divide.BackColor = Color.DimGray;
+            }
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            job = 1;
+            jobLabel.Text = "It's Minus";
+            if (!(NumberBox1.Text == "")) { number1 = Convert.ToInt32(NumberBox1.Text); }
+            textBoxNumber.Text = Convert.ToString(number1);
+            NumberBox1.Text = null;
+            if (job == 1)
+            {
+                Plus.BackColor = Color.DimGray;
+                Minus.BackColor = Color.Aqua;
+                Multiply.BackColor = Color.DimGray;
+                Divide.BackColor = Color.DimGray;
+            }
+        }
+
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            job = 2;
+            jobLabel.Text = "It's Multiply";
+            if (!(NumberBox1.Text == "")) { number1 = Convert.ToInt32(NumberBox1.Text);
+            }
+            else { }
+            textBoxNumber.Text = Convert.ToString(number1);
+            NumberBox1.Text = null;
+            if (job == 2)
+            {
+                Plus.BackColor = Color.DimGray;
+                Minus.BackColor = Color.DimGray;
+                Multiply.BackColor = Color.Aqua;
+                Divide.BackColor = Color.DimGray;
+            }
+        }
+
+        private void Divide_Click(object sender, EventArgs e)
+        {
+            job = 3;
+            jobLabel.Text = "It's Divide";
+            if (!(NumberBox1.Text == "")) { number1 = Convert.ToInt32(NumberBox1.Text); }
+            textBoxNumber.Text = Convert.ToString(number1);
+            NumberBox1.Text = null;
+            if (job == 3)
+            {
+                Plus.BackColor = Color.DimGray;
+                Minus.BackColor = Color.DimGray;
+                Multiply.BackColor = Color.DimGray;
+                Divide.BackColor = Color.Aqua;
+            }
+        }
+        public void Calc()
+        {
+            switch (job)
+            {
+                case 0:
+                    Calc_Plus();
+                    break;
+                case 1:
+                    Calc_Minus();
+                    break;
+                case 2:
+                    Calc_Multiply();
+                    break;
+                case 3:
+                    Calc_Divide();
+                    break;
+            }
+        }
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Calc();
+                textBoxNumber.Text = Convert.ToString(result);
+                if (textBoxNumber.Text == "")
+                {
+                    isJobDone = true;
+                };
+            }
+            catch (Exception)
+            {
+            }
+            
+        }
+        public void Calc_Plus()
+        {
+            int x = Convert.ToInt32(NumberBox1.Text);
+            textBoxNumber.Text = Convert.ToString(number1);
+            result = x + number1;
+            NumberBox1.Text = Convert.ToString(result);
+        }
+        public void Calc_Minus()
+        {
+            textBoxNumber.Text = Convert.ToString(number1);
+            int x = Convert.ToInt32(NumberBox1.Text);
+            result = number1 - x;
+            NumberBox1.Text = Convert.ToString(result);
+        }
+        public void Calc_Multiply()
+        {
+            textBoxNumber.Text = Convert.ToString(number1);
+            int x = Convert.ToInt32(NumberBox1.Text);
+            result = x * number1;
+            NumberBox1.Text = Convert.ToString(result);
+        }
+        public void Calc_Divide()
+        {
+            textBoxNumber.Text = Convert.ToString(number1);
+            int x = Convert.ToInt32(NumberBox1.Text);
+            result = x / number1;
+            NumberBox1.Text = Convert.ToString(result);
+        }
+    }
+}
